2d6a3adbd2fc955edb817aa0d4a8ffbdef520810
Stulov Dima <dm-stulov@yandex.ru>
2020-04-05 14:57:10 +0300

    final version
    fix

---
+++ src/cmake-build-debug/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dmitrij/Desktop/src/archiver.cpp" "/Users/dmitrij/Desktop/src/cmake-build-debug/CMakeFiles/src.dir/archiver.cpp.o"
+  "/Users/dmitrij/Desktop/src/bitstring.cpp" "/Users/dmitrij/Desktop/src/cmake-build-debug/CMakeFiles/src.dir/bitstring.cpp.o"
+  "/Users/dmitrij/Desktop/src/huffman.cpp" "/Users/dmitrij/Desktop/src/cmake-build-debug/CMakeFiles/src.dir/huffman.cpp.o"
+  "/Users/dmitrij/Desktop/src/main_encoding.cpp" "/Users/dmitrij/Desktop/src/cmake-build-debug/CMakeFiles/src.dir/main_encoding.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../Lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
